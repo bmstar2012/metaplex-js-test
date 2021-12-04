37e799dd1e33a8f6c9c8efbd3820304016544878
bmstar <bmstar2012@gmail.com>
2021-12-04 16:33:16 +0800

    Added store

--- src/home.tsx
+++ src/home.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {useState, useMemo} from 'react';
 import './app.scss';
-import {Connection, programs} from '@metaplex/js';
+import {Connection, programs, actions, Wallet, NodeWallet, AnyPublicKey} from '@metaplex/js';
+
 import {Keypair, PublicKey, clusterApiUrl} from '@solana/web3.js';
 
 /// Wallet start
-import {useWallet, WalletProvider, ConnectionProvider} from '@solana/wallet-adapter-react';
-import {WalletModalProvider, WalletMultiButton, WalletDisconnectButton} from '@solana/wallet-adapter-react-ui';
+import {useWallet} from '@solana/wallet-adapter-react';
+import {WalletMultiButton, WalletDisconnectButton} from '@solana/wallet-adapter-react-ui';
+import { getPhantomWallet } from "@solana/wallet-adapter-wallets";
 
-const {Metadata} = programs.metadata;
+const { metaplex: { Store, AuctionManager }, metadata: { Metadata }, auction: { Auction }, vault: { Vault } } = programs;
+
 const connection = new Connection('devnet');
 const tokenPublicKey = 'Gz3vYbpsB2agTsAwedtvtTkQ1CG9vsioqLW3r9ecNpvZ';
 
@@ -18,6 +21,7 @@ export const HomeView = () => {
     const { publicKey: walletPubkey } = wallet;
 
     const [metadataToken, setMetadataToken] = useState(tokenPublicKey);
+    const [storeId, setStoreId] = useState('');
     const onSearchMetadata = async () => {
         try {
             const ownedMetadata = await Metadata.load(connection, tokenPublicKey);
@@ -32,21 +36,43 @@ export const HomeView = () => {
             console.log("onLookMyMetadata", wallet, walletPubkey);
             if (walletPubkey) {
                 const ownedMetadata = await Metadata.findByOwnerV2(connection, walletPubkey);
-                console.log("onLookMyMetadata", ownedMetadata);
+                ownedMetadata.forEach((metadata) => {
+                    console.log('metadata:', metadata.pubkey.toBase58(), metadata);
+                })
+                // console.log("onLookMyMetadata", ownedMetadata);
             }
         } catch {
             console.log("onLookMyMetadata", 'Failed to fetch metadata');
         }
     }
-    if (wallet.connected) {
-        console.log('wallet connected!');
-    } else {
-        console.log('wallet no connected!!!');
+
+    const onLookupStore = async () => {
+        if (!wallet) {
+            return;
+        }
+        // const metaplexWallet = new NodeWallet(wallet.)
+        try {
+            // const { storeId } = await actions.initStore({
+            //     connection,
+            //     wallet: new NodeWallet(wallet.publicKey),
+            // });
+            const storeId = await Store.getPDA(walletPubkey as AnyPublicKey);
+            console.log("onLookupStore", storeId);
+            setStoreId(storeId.toBase58());
+
+            const store = await Store.load(connection, storeId);
+            console.log("store data: ", store);
+
+            // Get existing store id
+            // const storeId = await Store.getPDA(publicKey as AnyPublicKey);
+        } catch {
+            console.log("onLookMyMetadata", 'Failed to fetch metadata');
+        }
     }
 
     return (
         <div className="App">
-            <WalletMultiButton />
+            <WalletMultiButton /><br/><br/>
             <WalletDisconnectButton />
             <div className={'container-metadata'}>
                 <input type={"text"} onChange={(e) => {
@@ -56,7 +82,11 @@ export const HomeView = () => {
                 <br/>
                 <br/>
                 <div>{wallet.publicKey?.toBase58()}</div>
-                <button onClick={onLookMyMetadata}>My metadata</button>
+                <button onClick={onLookMyMetadata} disabled={!wallet.connected}>My metadata</button>
+                <br/>
+                <button onClick={onLookupStore} disabled={!wallet.connected}>Check store</button><br/>
+                <br/>
+                Store: {storeId}<br/>
                 <br/>
 
             </div>
